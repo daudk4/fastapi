e50f55fde204e7569fbceed8b96be630c20154bd
Daud Ali Khan <daudu6944@gmail.com>
2026-01-05 01:37:54 +0500

    ADD: connection

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Server is starting")
+    await init_db()
+    yield
+    print(f"Server has been stopped")
+
 
 version = "v1"
 app = FastAPI(
     version=version,
     title="Bookly",
     description="A REST API for a book review web service",
+    lifespan=life_span,
 )
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+Config = Settings()
