712f6bf16ba8d5bf481453bfa74373cbbe478fc1
Daud Ali Khan <daudu6944@gmail.com>
2026-01-07 00:09:44 +0500

    UPDATE: model issues replace with better approach now

--- src/books/models.py
+++ src/books/models.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as PG
+from sqlalchemy.sql import func
 from datetime import datetime
 import uuid
 
@@ -8,7 +9,7 @@ class Book(SQLModel, table=True):
     __tablename__ = "books"
     uid: uuid.UUID = Field(
         sa_column=Column(
-            PG.UUID,
+            PG.UUID(as_uuid=True),
             nullable=False,
             primary_key=True,
             default=uuid.uuid4,
@@ -20,8 +21,21 @@ class Book(SQLModel, table=True):
     published_date: str
     page_count: int
     language: str
-    created_at: datetime = Field(sa_column=Column(PG.TIMESTAMP, default=datetime.now()))
-    updated_at: datetime = Field(sa_column=Column(PG.TIMESTAMP, default=datetime.now()))
+    created_at: datetime = Field(
+        sa_column=Column(
+            PG.TIMESTAMP(timezone=True),
+            server_default=func.now(),
+            nullable=False,
+        )
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(
+            PG.TIMESTAMP(timezone=True),
+            server_default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+        )
+    )
 
     def __repr__(self):
         return f"<Book {self.title}>"
