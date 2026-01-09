6b10e0d4f79a2a0fda74c642ac3452fd78fe59d9
Daud Ali Khan <daudu6944@gmail.com>
2026-01-09 21:49:22 +0500

    ADD:  comments about init_db and get_session

--- src/db/main.py
+++ src/db/main.py
@@ -8,14 +8,21 @@ from sqlmodel import SQLModel
 async_engine = create_async_engine(Config.DATABASE_URL, echo=True)
 
 
+# THIS FUNCTION IS RESPONSIBLE FOR CREATING TABLES IN DATABASE
 async def init_db() -> None:
     async with async_engine.begin() as conn:
+        # IMPORTING MODELS INSIDE THE FUNCTION IS IMPORTANT BECAUSE SQLModel ONLY KNOWS ABOUT MODELS THAT HAVE BEEN IMPORTED.
         from src.books.models import Book
 
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
+# THIS FUNCTION IS HOW YOUR API GETS A DB CONNECTION FOR EACH REQUEST.
+# WITHOUT THE SESSION YOU CANNOT QUERY THE DATABASE.
+# WITHOUT THE SESSION YOU CANNOT INSERT/UPDATE/DELETE DATA FROM DB.
+# WITHOUT THIS YOU CANNOT USE SQLMODEL ORM
 async def get_session() -> AsyncSession:
+    # SESSIONMAKER IS A FACTORY. IT DOES NOT CREATE A SESSION YET. IT CREATES A FUCNTION THAT CAN CREATE SESSION.
     Session = sessionmaker(
         bind=async_engine, class_=AsyncSession, expire_on_commit=False
     )
