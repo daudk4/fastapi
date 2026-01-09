df26d26e1a7208fa4631d070c33a68f9c5594a32
Daud Ali Khan <daudu6944@gmail.com>
2026-01-09 20:43:50 +0500

    ADD: session class using sqlalchemy session maker

--- src/db/main.py
+++ src/db/main.py
@@ -1,13 +1,21 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 from src.config import Config
 from sqlmodel import SQLModel
 
 
-engine = create_async_engine(Config.DATABASE_URL, echo=True)
+async_engine = create_async_engine(Config.DATABASE_URL, echo=True)
 
 
-async def init_db():
-    async with engine.begin() as conn:
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
         from src.books.models import Book
 
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=async_engine, class_=AsyncSession, expire_on_commit=False
+    )
