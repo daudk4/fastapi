6487fd107fbd915fad55cfd81bc78a2558c70a53
Daud Ali Khan <daudu6944@gmail.com>
2026-01-09 23:36:21 +0500

    inject session in get_all_books and create_book route handlers

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,32 +1,28 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
+import sqlmodel.ext.asyncio.session
 from src.books.schemas import Book, BookUpdateModel
-from src.books.book_data import books_db
+from src.books.service import BookService
+from src.db.main import get_session
 from typing import List
 
 book_router = APIRouter()
+book_service = BookService()
 
 
 # GET ALL BOOKS
 @book_router.get("/", response_model=List[Book])
-async def get_all_books():
-    return books_db
+async def get_all_books(session: AsyncSession = Depends(get_session)):
+    books = await book_service.get_all_books(session)
+    return books
 
 
 # CREATE BOOK
-@book_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_book(book_data: Book) -> dict:
-    new_book = book_data.model_dump()
-
-    # CHECK IF BOOK ID ALREADY EXISTS
-    for book in books_db:
-        if book["id"] == new_book["id"]:
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail={"message": "Book ID already exists!"},
-            )
-
-    books_db.append(new_book)
-    return {"message": "Book added successfully"}
+@book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
+async def create_book(
+    book_data: Book, session: AsyncSession = Depends(get_session)
+) -> dict:
+    new_book = book_service.create_book(book_data, session)
+    return new_book
 
 
 # GET BOOK BY ID
