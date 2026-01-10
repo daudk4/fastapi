3ed51960a33e495958aeae2c79325933d3e28099
Daud Ali Khan <daudu6944@gmail.com>
2026-01-10 17:36:55 +0500

    UPDATE: create bok payload

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
-from src.books.schemas import Book, BookUpdateModel
+from src.books.schemas import Book, BookCreateModel, BookUpdateModel
 from src.books.service import BookService
 from src.db.main import get_session
 from typing import List
@@ -19,7 +19,7 @@ async def get_all_books(session: AsyncSession = Depends(get_session)):
 # CREATE BOOK
 @book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
 async def create_book(
-    book_data: Book, session: AsyncSession = Depends(get_session)
+    book_data: BookCreateModel, session: AsyncSession = Depends(get_session)
 ) -> dict:
     new_book = await book_service.create_book(book_data, session)
     return new_book
